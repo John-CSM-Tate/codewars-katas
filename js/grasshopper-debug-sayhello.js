ab7c1c5c0b1dc77b8c9c6e0fd22b6b9fa0006328
John Tate <14020610+John-CSM-Tate@users.noreply.github.com>
2022-11-17 21:27:50 +0000

    Added sayHello

---
+++ js/grasshopper-debug-sayhello.js
@@ -0,0 +1,10 @@
+//// DESCRIPTION:
+// The starship Enterprise has run into some problem when creating a program to greet 
+// everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Examples (Input ==> Output):
+// "Mr. Spock" -> "Hello, Mr. Spock"
+
+function sayHello(name) {
+    return 'Hello, ' + name
+  }
